3c47a30b489284cd3b5ae77cd0d987f383019a1e
Floflote <florent13a@gmail.com>
2022-12-19 19:13:31 +0100

    Navbar-and-scrolltotop

---
+++ Code/P-index/script-index.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict";
+
+  document
+    .querySelector("#navbarSideCollapse")
+    .addEventListener("click", function () {
+      document.querySelector(".offcanvas-collapse").classList.toggle("open");
+    });
+})();
